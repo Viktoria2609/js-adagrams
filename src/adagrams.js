7ccf3f7798197b900006af19685e4e79b0f9fc15
Viktoriia <lyudkevych.v@gmail.com>
2025-05-30 07:44:13 -0700

    Function scoreWord created and all related tests passed(Wave 3 complete )

--- src/adagrams.js
+++ src/adagrams.js
@@ -7,6 +7,18 @@ const LETTER_POOL = {
   Z: 1,
 };
 
+const SCORE_CHART = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10
+};
+
+const ADDITIONAL_POINTS = 8;
+
 export const drawLetters = () => {
   const allLetters = [];
   for (const letter in LETTER_POOL) {
@@ -46,8 +58,17 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
-};
+    let totalScore = 0;
+    for(let i = 0; i < word.length; i++){
+      let char = word[i].toUpperCase();
+      totalScore += SCORE_CHART[char] || 0;
+    }
+
+    if (word.length >= 7 && word.length <= 10) {
+    totalScore += ADDITIONAL_POINTS;
+  }
+    return totalScore;
+  };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
